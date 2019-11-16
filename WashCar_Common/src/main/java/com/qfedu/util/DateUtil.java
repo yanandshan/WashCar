6941d261cd1eb1485fa8b32946bced73c8b609af
zhaoyan <393315852@qq.com>
2019-11-16 16:49:42 +0800

    1.1

--- WashCar_Common/src/main/java/com/qfedu/util/DateUtil.java
+++ WashCar_Common/src/main/java/com/qfedu/util/DateUtil.java
@@ -19,5 +19,6 @@ public class DateUtil {
 
     public static void main(String[] args) {
         System.out.println(getTime(-7));
+        System.out.println("***************************");
     }
 }
